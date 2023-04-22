a249b276d629c51940caf8fe5dd86edbb21efcff
Rentluvs <tadatthanh161@gmail.com>
2023-04-22 21:36:30 +0700

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -107,11 +107,7 @@
         </q-form>
     </div>
     <div class="col-12 col-sm-6">
-    <q-img
-          class="q-ma-sm full-wdith"
-          src="https://m.media-amazon.com/images/I/91TrVCCNN0L._SX1200_.jpg"
-          :ratio="16/9"
-        />
+<p>gggg</p>
   </div>
     <div class="col-12 col-sm-2"></div>
 </div>
